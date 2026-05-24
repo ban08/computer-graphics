d6a4e9d333f6ea879d97a07d6ed6236dac954d84
YSAlba <yagoalba2005@gmail.com>
2026-05-24 15:32:58 +0100

    restored all elements.

--- project/MyScene.js
+++ project/MyScene.js
@@ -103,16 +103,16 @@ export class MyScene extends CGFscene {
 		this.lights[0].update();
 		this.lights[1].update();
 
-		//if (this.displayAxis) this.axis.display();
+		if (this.displayAxis) this.axis.display();
 
 		this.setDefaultAppearance();
 
 		this.sky.display();
 		this.sun.display();
 		this.clouds.display();
-		//this.terrain.display();
-		//this.grass.display();
-		//this.scatterElements.display();
-		//this.wagon.display();
+		this.terrain.display();
+		this.grass.display();
+		this.scatterElements.display();
+		this.wagon.display();
   	}
 }
