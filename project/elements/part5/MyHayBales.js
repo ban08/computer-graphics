91f55dc137b40375ca55067ada80b8c238f50699
sararibeiro24 <sara.m.ribeiro22@gmail.com>
2026-05-25 12:50:41 +0100

    separated bale object logic from the placement

--- project/elements/part5/MyHayBales.js
+++ project/elements/part5/MyHayBales.js
@@ -1,6 +1,6 @@
-import { CGFobject, CGFappearance, CGFtexture } from '../../../lib/CGF.js';
-import { MyTexturedBox } from '../../primitives/MyTexturedBox.js';
+import { CGFobject } from '../../../lib/CGF.js';
 import { PlacementGenerator } from '../../utils/PlacementProceduralGenerator.js';
+import { MyHayBale } from '../../objects/MyHayBale.js';
 
 /**
  * MyHayBales
@@ -23,38 +23,12 @@ export class MyHayBales extends CGFobject {
         this.flowers = flowers;
         this.rocks = rocks;
         this.placements = [];
-
-
-        this.bale = new MyTexturedBox(this.scene, 0.62, 0.34, 0.50, 0.28);
-        this.baleBandX = new MyTexturedBox(this.scene, 0.68, 0.04, 0.055, 0.20);
-        this.baleBandZ = new MyTexturedBox(this.scene, 0.055, 0.04, 0.56, 0.20);
-
-
-        this.initMaterials();
+        this.bale = new MyHayBale(this.scene);
 
 
         this.generateBalePlacements();
     }
 
-    initMaterials() {
-     
-        this.hay = new CGFappearance(this.scene);
-        this.hay.setAmbient(0.70, 0.50, 0.18, 1);
-        this.hay.setDiffuse(0.95, 0.72, 0.28, 1);
-        this.hay.setSpecular(0.10, 0.08, 0.03, 1);
-        this.hay.setShininess(6);
-        this.hay.setTexture(new CGFtexture(this.scene, './textures/wagonHay.svg'));
-        this.hay.setTextureWrap('REPEAT', 'REPEAT');
-
- 
-        this.leather = new CGFappearance(this.scene);
-        this.leather.setAmbient(0.20, 0.10, 0.05, 1);
-        this.leather.setDiffuse(0.34, 0.18, 0.09, 1);
-        this.leather.setSpecular(0.16, 0.08, 0.04, 1);
-        this.leather.setShininess(22);
-        this.leather.setTexture(new CGFtexture(this.scene, './textures/wagonLeather.svg'));
-        this.leather.setTextureWrap('REPEAT', 'REPEAT');
-    }
 
     randomRange(min, max) {
         return min + Math.random() * (max - min);
@@ -133,14 +107,6 @@ export class MyHayBales extends CGFobject {
     }
 }
 
-    drawAt(object, x, y, z) {
-        this.scene.pushMatrix();
-        this.scene.translate(x, y, z);
-        object.display();
-        this.scene.popMatrix();
-    }
-
-
     display() {
         for (const placement of this.placements) {
             this.scene.pushMatrix();
@@ -148,14 +114,8 @@ export class MyHayBales extends CGFobject {
             this.scene.translate(placement.x, placement.y + 0.17, placement.z);
             this.scene.rotate(placement.rotation, 0, 1, 0);
 
-            this.hay.apply();
             this.bale.display();
 
-            this.leather.apply();
-            this.drawAt(this.baleBandX, 0, 0.18, -0.14);
-            this.drawAt(this.baleBandX, 0, 0.18, 0.14);
-            this.drawAt(this.baleBandZ, -0.17, 0.19, 0);
-            this.drawAt(this.baleBandZ, 0.17, 0.19, 0);
 
             this.scene.popMatrix();
         }
---
+++ project/objects/MyHayBale.js
@@ -0,0 +1,62 @@
+import { CGFobject, CGFappearance, CGFtexture } from '../../../lib/CGF.js';
+import { MyTexturedBox } from '../primitives/MyTexturedBox.js';
+
+/**
+ * MyHayBale
+ * Object representing a single standalone hay bale with leather bands.
+ */
+export class MyHayBale extends CGFobject {
+    /**
+     * @constructor
+     * @param {CGFscene} scene - Reference to the main scene
+     */
+    constructor(scene) {
+        super(scene);
+
+        this.bale = new MyTexturedBox(this.scene, 0.62, 0.34, 0.50, 0.28);
+        this.baleBandX = new MyTexturedBox(this.scene, 0.68, 0.04, 0.055, 0.20);
+        this.baleBandZ = new MyTexturedBox(this.scene, 0.055, 0.04, 0.56, 0.20);
+
+        this.initMaterials();
+    }
+
+
+    initMaterials() {
+     
+        this.hay = new CGFappearance(this.scene);
+        this.hay.setAmbient(0.70, 0.50, 0.18, 1);
+        this.hay.setDiffuse(0.95, 0.72, 0.28, 1);
+        this.hay.setSpecular(0.10, 0.08, 0.03, 1);
+        this.hay.setShininess(6);
+        this.hay.setTexture(new CGFtexture(this.scene, './textures/wagonHay.svg'));
+        this.hay.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.leather = new CGFappearance(this.scene);
+        this.leather.setAmbient(0.20, 0.10, 0.05, 1);
+        this.leather.setDiffuse(0.34, 0.18, 0.09, 1);
+        this.leather.setSpecular(0.16, 0.08, 0.04, 1);
+        this.leather.setShininess(22);
+        this.leather.setTexture(new CGFtexture(this.scene, './textures/wagonLeather.svg'));
+        this.leather.setTextureWrap('REPEAT', 'REPEAT');
+    }
+
+
+    drawAt(object, x, y, z) {
+        this.scene.pushMatrix();
+        this.scene.translate(x, y, z);
+        object.display();
+        this.scene.popMatrix();
+    }
+
+    display() {
+        this.hay.apply();
+        this.bale.display();
+
+
+        this.leather.apply();
+        this.drawAt(this.baleBandX, 0, 0.18, -0.14);
+        this.drawAt(this.baleBandX, 0, 0.18, 0.14);
+        this.drawAt(this.baleBandZ, -0.17, 0.19, 0);
+        this.drawAt(this.baleBandZ, 0.17, 0.19, 0);
+    }
+}
